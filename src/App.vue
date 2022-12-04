af9e49e154abcd16e7c8383365cc361366f1e980
rox perron <roxanneperron11@gmail.com>
2022-12-04 16:13:33 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+   <h1>Bienvenue dans la To Do Liste</h1>
+   <ToDoForm />
+</template>
+
+<script>
+import ToDoForm from './components/ToDoForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ToDoForm,
+  }
+}
+</script>
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,37 @@
+<template>
+    <input type="text" v-model="userToDo" />
+    <button v-on:click="addToDo">Ajouter un To Do</button>
+
+    <ul>
+       <li 
+       v-for="todo in todos" 
+       v-on:click="deleteToDo(todo)">{{todo}}</li>
+    </ul>
+</template>
+
+
+<script>
+  export default {
+  data() {
+    return {
+        todos : ["Learnig Vue 3", "Installing Bootstrap in my app", "Make it beautiful with CSS"],
+        userToDo: '',
+    
+    }
+  },
+  methods: {
+    addToDo(){
+        this.todos.push(this.userToDo);
+    },
+
+    deleteToDo(todo){
+        let position = this.todos.indexOf(todo);
+        this.todos.splice(position, 1);
+    },
+
+
+  
+  }
+}
+
+</script>
